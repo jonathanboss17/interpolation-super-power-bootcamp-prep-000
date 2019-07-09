28d31a676683432e188b2619c7f45dea00cc27de
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-09 19:36:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,7 +7,13 @@ def display_rainbow(colors)
   letters = ["R: ", "O: ", "Y: ", "G: ", "B: ", "I: ", "V: "]
   
   7.times do |i|
-    print letters[i] + colors[i] + ","
+    
+    if(i == colors.length -1)
+      print letter[i] + colors[i]
+    end
+    
+    print letters[i] + colors[i] + ", "
+
   end
   
 end
