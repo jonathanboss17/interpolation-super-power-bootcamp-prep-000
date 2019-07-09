83a1ecbc4ac88520cf8ac8ba34a575571cd986c9
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-09 19:31:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,8 +4,6 @@ colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow(colors)
   
-  letters = ["R: ", "O: ", "Y: ", "G: ", "B: ", "I: ", "V: "]
-  
   for i in colors do 
     pr
   
